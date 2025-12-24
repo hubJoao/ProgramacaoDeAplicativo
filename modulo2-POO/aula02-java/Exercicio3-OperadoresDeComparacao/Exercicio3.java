8eccdb4bdcfcfa8d74c2b4b2eb3ce021d122a906
hubJoao <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-12-24 16:03:13 -0300

    resolução do exercicio 3

---
+++ modulo2-POO/aula02-java/Exercicio3-OperadoresDeComparacao/Exercicio3.java
@@ -0,0 +1,13 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        int idade = 25;
+
+        //TODO: Realize as comparações
+        System.out.println("idade == 25: " + (idade == 25));
+        System.out.println("idade != 30: " + (idade != 30));
+        System.out.println("idade > 18: " + (idade > 18));
+        System.out.println("idade < 30: " + (idade < 30));
+        System.out.println("idade >= 25: " + (idade >= 25));
+        System.out.println("idade <= 25: " + (idade <= 25));
+    }
+}

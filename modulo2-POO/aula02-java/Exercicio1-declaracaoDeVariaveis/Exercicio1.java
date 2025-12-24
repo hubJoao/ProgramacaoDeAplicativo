17599052c97d972ab6fb8063d96449e9fbef1b9a
hubJoao <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-12-24 14:44:51 -0300

    organizando arquivos em suas respectivas pastas

---
+++ modulo2-POO/aula02-java/Exercicio1-declaracaoDeVariaveis/Exercicio1.java
@@ -0,0 +1,38 @@
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        // TODO: Declare as variáveis
+        int idade = 25;
+        double altura = 1.75;
+        String nome = "Joao";
+        boolean ativo = true;
+        char inicial = 'J';
+
+        // 3 variáveis extras
+        double salarioMensal = 10000.50;
+        long salarioAnual = 120004L;
+        float bonusPercentual = 7.5f;
+
+        // TODO: Exiba os valores
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Nome: " + nome);
+        System.out.println("Ativo: " + ativo);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("Salário mensal: " + salarioMensal);
+        System.out.println("Salário anual: " + salarioAnual);
+        System.out.println("Percentual de bônus: " + bonusPercentual + "%");
+    }
+}
+
+
+
+
+
+//Resultado Esperado:
+// Idade: 25
+//Altura: 1.75
+//Nome: João Silva
+//Ativo: true
+//Inicial: J
+//Desafio Extra: Declare mais 3 variáveis de tipos diferentes e exiba-as.

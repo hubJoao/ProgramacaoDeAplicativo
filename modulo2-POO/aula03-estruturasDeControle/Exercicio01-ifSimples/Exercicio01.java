a554af41dfce067f68ca809984bf2e7fe3db739a
hubJoao <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-12-26 12:57:54 -0300

    resolução do exercio01 aula 3

---
+++ modulo2-POO/aula03-estruturasDeControle/Exercicio01-ifSimples/Exercicio01.java
@@ -0,0 +1,40 @@
+    public class Exercicio01{
+        public static void main(String[] args){
+          int idade=25;
+            
+            //TODO: Verifique se é maior de idade
+                if(idade>=18){
+
+                    System.out.println("É maior de idade");
+                }
+
+            //TODO: Verifique se pode votar
+               if(idade>=16){
+                    System.out.println("Você pode votar");       
+                }
+            
+            //TODO: Verifique se é idoso
+                if(idade>=60){
+                    System.out.println("Você é idoso");
+                }
+
+            
+
+            //TODO: Verifique se pode tirar CNH
+                if(idade>=18){
+                    System.out.println("pode tira a carteira");
+                }
+
+
+            //TODO: Verifique se pode ingerir bebida alcólica
+                if(idade>=18){
+                    System.out.println("pode beber ");
+                } 
+
+            //TODO: Verifique se pode se aposentar
+                if(idade>=65){
+                    System.out.println("pode se aposentar ");
+                }          
+       }
+
+    }

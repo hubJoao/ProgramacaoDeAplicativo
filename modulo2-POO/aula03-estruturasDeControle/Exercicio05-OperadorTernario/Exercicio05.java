1e8738fc8a92e04b1896b93aaad8f65ce1a5210e
hubjoao <antonio_j_pessoa@estudante.sesisenai.org.br>
2025-12-29 11:13:18 -0300

    correções

--- modulo2-POO/aula03-estruturasDeControle/Exercicio05-OperadorTernario/Exercicio05.java
+++ modulo2-POO/aula03-estruturasDeControle/Exercicio05-OperadorTernario/Exercicio05.java
@@ -10,13 +10,13 @@ public class Exercicio05{
         System.out.println("Categoria: "+ categoria);
 
          //TODO: Classifique como par ou ímpar
-         int numero=25;
-         String tipo = (idade%2 ==0)? " Par ":" Impar";
+         int numero=20;
+         String tipo = (numero%2 ==0)? " Par ":" Impar";
          System.out.println("Tipo:"+tipo);
 
          //TODO: Calcule desconto
          double preco=100;
-         double desconto = (preco<50)? preco*0.10 : 0;
+         double desconto = (preco>50)? preco*0.10 : 0;
          
          System.out.println("Desconto de R$: "+desconto);
 
@@ -24,14 +24,14 @@ public class Exercicio05{
         //#Desafio Extra: Use ternário para:     
           
        // Classificar como jovem (< 30) ou adulto
-        int idade2=35;
+        int idade2=28;
         String faixaEtaria = (idade2<30)?"Jovem":"Adulto";
         System.out.println("Faixa etária: "+faixaEtaria);
 
        // Classificar como positivo, negativo ou zero
         int numero2 = 0;
         String negativoPositivo=(numero2<0 )?"Negativo":(numero2>0)?"Positivo":"zero";
-        System.out.println("o nunero é: "+negativoPositivo);
+        System.out.println("o numero é: "+negativoPositivo);
        
 
        // Calcular taxa de juros baseada em valor
